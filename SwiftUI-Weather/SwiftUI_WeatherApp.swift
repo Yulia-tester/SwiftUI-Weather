14fcbb327b195c2462b5f4e7f63db1e33082ec74
Юлия Дегтярева <yuliia.hlavatska@gmail.com>
2025-03-04 18:49:08 -0600

    "first commit"

---
+++ SwiftUI-Weather/SwiftUI_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherApp.swift
+//  SwiftUI-Weather
+//
+//  Created by Юлия Дегтярева on 2025-03-04.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
